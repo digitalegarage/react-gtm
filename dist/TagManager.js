b111c2807d72e1e754305404fffe4a8060a41649
Marcel Sebald <marcel.sebald@pub.tech>
2023-09-12 13:06:16 +0200

    chore: update dist folder

--- dist/TagManager.js
+++ dist/TagManager.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var _Snippets = require('./Snippets');
+var _Snippets = require("./Snippets");
 
 var _Snippets2 = _interopRequireDefault(_Snippets);
 
@@ -8,7 +8,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var TagManager = {
   dataScript: function dataScript(dataLayer) {
-    var script = document.createElement('script');
+    var script = document.createElement("script");
     script.innerHTML = dataLayer;
     return script;
   },
@@ -16,13 +16,13 @@ var TagManager = {
     var snippets = _Snippets2.default.tags(args);
 
     var noScript = function noScript() {
-      var noscript = document.createElement('noscript');
+      var noscript = document.createElement("noscript");
       noscript.innerHTML = snippets.iframe;
       return noscript;
     };
 
     var script = function script() {
-      var script = document.createElement('script');
+      var script = document.createElement("script");
       script.innerHTML = snippets.script;
       return script;
     };
@@ -38,16 +38,16 @@ var TagManager = {
   initialize: function initialize(_ref) {
     var gtmId = _ref.gtmId,
         _ref$gtmSrc = _ref.gtmSrc,
-        gtmSrc = _ref$gtmSrc === undefined ? 'https://www.googletagmanager.com' : _ref$gtmSrc,
+        gtmSrc = _ref$gtmSrc === undefined ? "https://tm.br.de" : _ref$gtmSrc,
         _ref$events = _ref.events,
         events = _ref$events === undefined ? {} : _ref$events,
         dataLayer = _ref.dataLayer,
         _ref$dataLayerName = _ref.dataLayerName,
-        dataLayerName = _ref$dataLayerName === undefined ? 'dataLayer' : _ref$dataLayerName,
+        dataLayerName = _ref$dataLayerName === undefined ? "dataLayer" : _ref$dataLayerName,
         _ref$auth = _ref.auth,
-        auth = _ref$auth === undefined ? '' : _ref$auth,
+        auth = _ref$auth === undefined ? "" : _ref$auth,
         _ref$preview = _ref.preview,
-        preview = _ref$preview === undefined ? '' : _ref$preview;
+        preview = _ref$preview === undefined ? "" : _ref$preview;
 
     var gtm = this.gtm({
       id: gtmId,
@@ -65,7 +65,7 @@ var TagManager = {
   dataLayer: function dataLayer(_ref2) {
     var _dataLayer = _ref2.dataLayer,
         _ref2$dataLayerName = _ref2.dataLayerName,
-        dataLayerName = _ref2$dataLayerName === undefined ? 'dataLayer' : _ref2$dataLayerName;
+        dataLayerName = _ref2$dataLayerName === undefined ? "dataLayer" : _ref2$dataLayerName;
 
     if (window[dataLayerName]) return window[dataLayerName].push(_dataLayer);
     var snippets = _Snippets2.default.dataLayer(_dataLayer, dataLayerName);
