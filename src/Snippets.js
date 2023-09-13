5c617cf7578f7c78a387f1adba022e25b9219ccd
Marcel Sebald <marcel.sebald@pub.tech>
2023-09-13 10:15:41 +0200

    fix: try to fix SyntaxError: Unexpected string

--- src/Snippets.js
+++ src/Snippets.js
@@ -21,6 +21,16 @@ const Snippets = {
       <iframe src="${gtm_src}/ns.html?id=${id}${gtm_auth}${gtm_preview}"
         height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>`;
 
+    // const script = `
+    //   (function(w,d,s,l,i){w[l]=w[l]||[];
+    //     w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(
+    //       events
+    //     ).slice(1, -1)}});
+    //     var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    //     j.async=true;j.src='${gtm_src}/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
+    //     f.parentNode.insertBefore(j,f);
+    //   })(window,document,'script','${dataLayerName}','${id}');`;
+
     const script = `(function (w, d, s, l, i) {
         w[l] = w[l] || [];
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(
